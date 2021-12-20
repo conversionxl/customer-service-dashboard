725dfe99ac0821a1b24d527158839dc4b1f7f79b
Andrew Noblet <andrewbnoblet@gmail.com>
2021-12-20 12:47:09 -0500

    feat(dashboard): change test config to use lib instead of src

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,9 +1,7 @@
 import { visualRegressionPlugin } from "@web/test-runner-visual-regression/plugin";
-import { esbuildPlugin } from "@web/dev-server-esbuild";
 
 export default {
     plugins: [
-        esbuildPlugin({ ts: true }),
         visualRegressionPlugin({
             update: process.argv.includes("--update-visual-baseline"),
         }),
