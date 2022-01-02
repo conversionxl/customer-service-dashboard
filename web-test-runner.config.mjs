f630391a6eb1017f74050dc6f2cfaa9c9f5512b8
Andrew Noblet <andrewbnoblet@gmail.com>
2022-01-02 12:04:44 -0500

    wip

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,6 +1,15 @@
 import { visualRegressionPlugin } from "@web/test-runner-visual-regression/plugin";
 
 export default {
+    testRunnerHtml: (testFramework) =>
+        `
+            <html>
+                <body>
+                    <script>window.localStorage.token = ""</script>
+                    <script type="module" src="${testFramework}"></script>
+                </body>
+            </html>
+        `,
     plugins: [
         visualRegressionPlugin({
             update: process.argv.includes("--update-visual-baseline"),
